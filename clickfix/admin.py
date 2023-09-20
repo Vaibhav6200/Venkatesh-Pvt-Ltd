950b0b56a55da74fcc5ecec62010c175d282f7ea
vaibhav <vaibhavpaliwal620@gmail.com>
2023-09-21 00:54:41 +0530

    .\requirements.txt Modified

--- clickfix/admin.py
+++ clickfix/admin.py
@@ -26,7 +26,13 @@ class CustomServices(admin.ModelAdmin):
 
 @admin.register(SubServices)
 class CustomSubServices(admin.ModelAdmin):
-    list_display = ['id', 'sub_service_name', 'service']
+    list_display = ['id', 'sub_service_name', 'service', 'sub_service_price']
     list_display_links = ['id', 'service', 'sub_service_name']
     search_fields = ['sub_service_name', 'service']
     list_filter = ['service']
+
+
+@admin.register(Testimonials)
+class CustomTestimonials(admin.ModelAdmin):
+    list_display = ['id', 'title', 'state']
+    list_display_links = ['id', 'title']
