374e336c90f99f92b8ba27896c88557a26b042ef
vaibhav <vaibhavpaliwal620@gmail.com>
2023-09-27 11:22:37 +0530

    Live Tracking Done

--- razorpayAPI/admin.py
+++ razorpayAPI/admin.py
@@ -4,14 +4,14 @@ from .models import *
 
 @admin.register(Order)
 class CustomOrder(admin.ModelAdmin):
-    list_display = ['id', 'user', 'session_id', 'total_cost', 'payment_status', 'booking_status', 'tracking_status', 'created_at']
+    list_display = ['id', 'user', 'session_id', 'total_cost', 'payment_status', 'booking_status', 'tracking_status', 'service_provider', 'created_at']
     list_display_links = ['id', 'user', 'session_id']
     search_fields = ['id', 'user', 'session_id', 'payment_status', 'created_at']
     list_filter = ['payment_status']
 
 
 @admin.register(OrderItem)
-class CustomOrder(admin.ModelAdmin):
+class CustomOrderItem(admin.ModelAdmin):
     list_display = ['id', 'order', 'sub_service', 'quantity', 'start_date', 'time_slot', 'created_at']
     list_display_links = ['id', 'order', 'sub_service']
     search_fields = ['id', 'order', 'sub_service', 'start_date', 'time_slot', 'created_at']
